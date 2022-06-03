33d965a986b453a7f592aad4b2fe60129ed1094c
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-06-03 15:39:06 -0300

    Initial Commit

---
+++ Facebook-Feed-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook-Feed-App
+//
+//  Created by Cecilia Andrea Pesce on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facebook-Feed-App/Facebook_Feed_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_Feed_AppApp.swift
+//  Facebook-Feed-App
+//
+//  Created by Cecilia Andrea Pesce on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_Feed_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
