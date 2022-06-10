cc3fd0ff71e0103056b35d197142abe18c1cf878
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-06-09 21:58:25 -0300

    add gradient layers

--- Facebook-Feed-App/MainController.swift
+++ Facebook-Feed-App/MainController.swift
@@ -29,13 +29,90 @@ class PostCell: LBTAListCell<String> {
                      spacing: 8).padLeft(12).padRight(12).padTop(12),
         postTextLabel,
               photosGridController.view,
-              spacing:8
-        )
+              spacing:8)
     }
 }
 
-class MainController: LBTAListController<PostCell, String>,
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storiesController.view)
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    class StoryPhotoCell: LBTAListCell<String> {
+       
+        override var item: String! {
+            didSet{
+                imageView.image = UIImage(named:item)
+            }
+        }
+        
+        let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+        let nameLabel = UILabel(text: "Cecilia", font: .boldSystemFont(ofSize: 14), textColor: .white)
+        override func setupViews() {
+            imageView.layer.cornerRadius = 10
+            
+            stack(imageView)
+            
+            setupGradientLayer()
+            
+            stack(UIView(),nameLabel).withMargins(.allSides(8))
+        }
+        let gradientLayer = CAGradientLayer()
+        
+        fileprivate func setupGradientLayer(){
+            
+            gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+            gradientLayer.locations = [0.7,1.1]
+            layer.cornerRadius = 10
+            clipsToBounds = true
+            layer.addSublayer(gradientLayer)
+        }
+        override func layoutSubviews(){
+            super.layoutSubviews()
+            
+            gradientLayer.frame = bounds
+        }
+    }
+    
+class StoriesController: LBTAListController<StoryPhotoCell,String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        self.items = ["cat with sunglasses and cap","cat with sunglasses and cap", "Pink_flower", "Pink_flower", "Pink_flower", "Pink_flower"]
+    }
+    }
+    
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>,
     UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+        //aqui cria a barrinha cinza antes do primeiro post, mas na verdade encolhe o retangulo de cima?
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
